d4149819b751dc4ab5a6f9e3fe59d6d5d0642d6c
Jose Regalado <jowav467@hotmail.com>
2026-03-18 17:23:15 -0600

    updated correct redirect for when domain is being called

--- website/website/urls.py
+++ website/website/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from resume import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/home/', permanent=False)),
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
